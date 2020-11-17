68c180ad190330e7d9bdb8f0e53756ed4933b061
Duarte-Rodrigues <58471052+Duarte-Rodrigues@users.noreply.github.com>
2020-11-17 17:34:54 +0000

    second try (failed)

--- src/main.c
+++ src/main.c
@@ -1,65 +1,65 @@
-/*******************************************
- * Example 3
- *******************************************/
+// /*******************************************
+//  * Example 3
+//  *******************************************/
 
-#include <avr/io.h>
-#include <util/delay.h>
-#include "timer_tools.h"
-#include "serial_printf.h"
-#include <avr/interrupt.h>
+// #include <avr/io.h>
+// #include <util/delay.h>
+// #include "timer_tools.h"
+// #include "serial_printf.h"
+// #include <avr/interrupt.h>
 
-#define LED PB5
-
-
-// up to 8 servos on portB sequentially pulsed
-// uses timer1 in CTC mode
-// synchro gap is constant, 6 ms
-// frame is variable
-// min.frame = 8*1+6 = 14 ms
-// max.frame = 8*2+6 = 22 ms
-
-// Mega88, 8 MHz
+// #define LED PB5
 
 
-// initial pulse times in us for servo 0, 1, 2...7 (last value 6000 is the synchro gap)
-unsigned int servo[2] = {1500, 6000};
+// // up to 8 servos on portB sequentially pulsed
+// // uses timer1 in CTC mode
+// // synchro gap is constant, 6 ms
+// // frame is variable
+// // min.frame = 8*1+6 = 14 ms
+// // max.frame = 8*2+6 = 22 ms
 
-//----------------------------------------------------------------
-
-ISR(TIMER1_COMPA_vect)
-{
-static unsigned char servo_num;
-
-   PORTB = (1<<(servo_num + 1 ));          // end pulse for servo (n), start pulse for servo (n+1)            
-   OCR1A = servo[servo_num];        // set width of pulse
-   servo_num++;                     // prepare next servo 
-   if(servo_num > 8) servo_num = 0; // again from servo 0;
-}
+// // Mega88, 8 MHz
 
 
-////////////////// MAIN //////////////////////////////
-int main (void)
-{
-      DDRB = 255;                        // portb output
-      TCCR1B |= (1<<WGM12) | (1<<CS11);  // pwm mode 4,CTC, prescale=8
-      TIMSK1 |= (1<<OCIE1A);             // enable T1_compareA interrupt 
-      TCNT1 = 65530;
-      sei();   
+// // initial pulse times in us for servo 0, 1, 2...7 (last value 6000 is the synchro gap)
+// unsigned int servo[3] = {1500, 1500, 6000};
 
-// test 
-      for(;;)                   
-      {
-         servo[0] = 1000;          // servos 0 and 1 left
-         servo[1] = 1000;
-         _delay_ms(2000);
+// //----------------------------------------------------------------
 
-         servo[0] = 1500;          // servos 0 and 1 center
-         servo[1] = 1500;
-         _delay_ms(2000);
+// ISR(TIMER1_COMPA_vect)
+// {
+// static unsigned char servo_num;
 
-         servo[0] = 2000;          // servos 0 and 1 right
-         servo[1] = 2000;
-         _delay_ms(2000);  
-      }
-}
-////////////////// END MAIN //////////////////////////
+//    PORTB = (1<<(servo_num + 1 ));          // end pulse for servo (n), start pulse for servo (n+1)            
+//    OCR1A = servo[servo_num];        // set width of pulse
+//    servo_num++;                     // prepare next servo 
+//    if(servo_num > 2) servo_num = 0; // again from servo 0;
+// }
+
+
+// ////////////////// MAIN //////////////////////////////
+// int main (void)
+// {
+//       DDRB = 255;                        // portb output
+//       TCCR1B |= (1<<WGM12) | (1<<CS11);  // pwm mode 4,CTC, prescale=8
+//       TIMSK1 |= (1<<OCIE1A);             // enable T1_compareA interrupt 
+//       TCNT1 = 65530;
+//       sei();   
+
+// // test 
+//       while(1)                   
+//       {
+//          servo[0] = 1000;          // servos 0 and 1 left
+//          servo[1] = 1000;
+//          _delay_ms(2000);
+
+//          servo[0] = 1500;          // servos 0 and 1 center
+//          servo[1] = 1500;
+//          _delay_ms(2000);
+
+//          servo[0] = 2000;          // servos 0 and 1 right
+//          servo[1] = 2000;
+//          _delay_ms(2000);  
+//       }
+// }
+// ////////////////// END MAIN //////////////////////////
---
+++ src/try_only_one.c
@@ -0,0 +1,29 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+
+    DDRB |= (1 << 1);
+    // PB1 is now an output - corresponds to OCR0A
+
+    TCCR1A |= (1 << COM1A1);
+    // set non-inverting mode
+
+    TCCR1A |= (1 << WGM01) | (1 << WGM00);
+    // set fast PWM Mode
+
+    ICR1 = 39999;
+
+    OCR1A = ICR1 - 2000;
+    // gives servo motor position
+
+    TCCR1B |= (1 << CS11);
+    // set prescaler to 8 and starts PWM
+
+
+    while(1)
+    {
+    }
+}
