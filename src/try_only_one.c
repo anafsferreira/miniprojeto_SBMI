937865e629040b508649982f535a4e0ce2c1e754
anafsferreira <72914997+anafsferreira@users.noreply.github.com>
2020-11-24 15:03:01 +0000

    try one servo

--- src/try_only_one.c
+++ src/try_only_one.c
@@ -1,29 +1,30 @@
-// #define F_CPU 16000000UL
-// #include <avr/io.h>
-// #include <util/delay.h>
-
-// int main(void)
-// {
-//     DDRB |= (1 << 1);
-//     // PB1 is now an output - corresponds to OCR0A
-
-//     TCCR1A |= (1 << COM1A1);
-//     // set non-inverting mode
-
-//     TCCR1A |= (1 << WGM01) | (1 << WGM00);
-//     // set fast PWM Mode
-
-//     ICR1 = 39999;
-
-//     OCR1A = ICR1 - 2000;
-//     // gives servo motor position
-
-//     TCCR1B |= (1 << CS11);
-//     // set prescaler to 8 and starts PWM
-
-
-//     while(1)
-//     {
-//         // fazer isto
-//     }
-// }
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+void Wait()
+{
+   uint16_t i;
+   for(i=0;i<50;i++)
+   {
+      _delay_loop_2(0);
+      _delay_loop_2(0);
+      _delay_loop_2(0);
+   }
+}
+void main()
+{
+  //FOR TIMER1
+   TCCR1A|=(1<<COM1A1)|(1<<COM1B1)|(1<<WGM11);        //NON Inverted PWM
+   TCCR1B|=(1<<WGM13)|(1<<WGM12)|(1<<CS11)|(1<<CS10); //PRESCALER=64 MODE 14(FAST PWM)
+   ICR1=4999;  //fPWM=50Hz 
+   DDRD|=(1<<PD4)|(1<<PD5);   //PWM Pins as Output
+   while(1)
+   {
+      OCR1A=316;  //90 degree
+      Wait();
+      OCR1A=97;   //0 degree
+      Wait();
+     OCR1A=535;  //180 degree
+      Wait();
+   }
+}
