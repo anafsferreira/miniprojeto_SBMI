fd32b341a121e803d96c8ef08a68e95155efed36
ana <anafsferreira2@gmail.com>
2020-12-06 16:07:59 +0000

    some changes

--- src/try_only_one.c
+++ src/try_only_one.c
@@ -1,82 +1,83 @@
 
-#define F_CPU 16000000
-#include <avr/io.h>
-#include <util/delay.h>
-#include "timer_tools.h"
-#include "serial_printf.h"
-#include <avr/interrupt.h>
+// #define F_CPU 16000000
+// #include <avr/io.h>
+// #include <util/delay.h>
+// #include "timer_tools.h"
+// #include "serial_printf.h"
+// #include <avr/interrupt.h>
 
-// segundo a datasheet roda no máximo 120
-// 4000 -2000 - > roda 90 graus / 45
-// 4100 - 1500 -> 90 + 15 
+// // segundo a datasheet roda no máximo 120
+// // 4000 -2000 - > roda 90 graus / 45
+// // 4100 - 1500 -> 90 + 15 
 
-#define MAX_SERVO 6000 // 2 ms
-#define MIN_SERVO 4000 // 1 ms
-#define MAX_POT 1023
-#define MIN_POT 0 
+// #define MAX_SERVO 4330 // 2 ms
+// #define MIN_SERVO 1670 // 1 ms
+// #define MAX_POT 1023
+// #define MIN_POT 0 
 
-void init_tc1(void){
-    TCCR1B = 0; // stop tc1
-    TIFR1 |=(7 << TOV1); // clear pending intrpt
-    /* Set Fast PWM non-inverting mode */
-	TCCR1A = (1 << WGM11) | (1 << COM1A1);
-	TCCR1B |= (1 << WGM12) | (1 << WGM13) ;
-    //init counter
-    TCNT1 = 0;
-    ICR1 = 40000; // 20 ms
+// void init_tc1(void){
+//     TCCR1B = 0; // stop tc1
+//     TIFR1 |=(7 << TOV1); // clear pending intrpt
+//     /* Set Fast PWM non-inverting mode */
+// 	TCCR1A = (1 << WGM11) | (1 << COM1A1);
+// 	TCCR1B |= (1 << WGM12) | (1 << WGM13) ;
+//     //init counter
+//     TCNT1 = 0;
+//     ICR1 = 40000; // 20 ms
 
-    TIMSK1 |= (1 << OCIE0A);
-    // Prescaler 8
-    TCCR1B |= (1 << CS11);
+//     TIMSK1 |= (1 << OCIE0A);
+//     // Prescaler 8
+//     TCCR1B |= (1 << CS11);
 
-}
+// }
 
-void init_adc(void){
-    ADMUX |= (1 << REFS0); // definir Vref
-    ADCSRA |= (7 << ADPS0); // prescaler 128
-    ADCSRA |= (1 << ADEN); // ativar adc
-    DIDR0 |= (1 << PC0); // desativar o buffer digital
+// void init_adc(void){
+//     ADMUX |= (1 << REFS0); // definir Vref
+//     ADCSRA |= (7 << ADPS0); // prescaler 128
+//     ADCSRA |= (1 << ADEN); // ativar adc
+//     DIDR0 |= (1 << PC0); // desativar o buffer digital
 
-}
+// }
 
-unsigned int read_ADC(uint8_t channel){
-    /* Seleciona canal e referência */
-    ADMUX = (ADMUX & 0xF0)  | (channel & 0x0F);
+// unsigned int read_ADC(uint8_t channel){
+//     /* Seleciona canal e referência */
+//     ADMUX = (ADMUX & 0xF0)  | (channel & 0x0F);
 
-    ADCSRA |= (1 << ADSC); // iniciar a conversao em modo manual
+//     ADCSRA |= (1 << ADSC); // iniciar a conversao em modo manual
 
-    while(ADCSRA & ( 1 << ADSC)); // esperar pelo fim da conversão
+//     while(ADCSRA & ( 1 << ADSC)); // esperar pelo fim da conversão
 
-    return ADC;
-}
+//     return ADC;
+// }
 
 
 
-int main(void){
-    uint16_t adc_value  = 0, tmp = 0;
-    float m = 0;
-    uint16_t b = MIN_SERVO;
-    m = (float)(MAX_SERVO - MIN_SERVO)/(float)MAX_POT;
-    DDRB |= (1 << PB1); // pin 9 arduino - servo
+// int main(void){
+//     uint16_t adc_value  = 0, tmp = 0;
+//     float m = 0;
+//     uint16_t b = MIN_SERVO;
+//     m = (float)(MAX_SERVO - MIN_SERVO)/(float)MAX_POT;
+//     DDRB |= (1 << PB1); // pin 9 arduino - servo
 
     
 
-    init_tc1();
-    init_adc();
-    printf_init();
+//     init_tc1();
+//     init_adc();
+//     printf_init();
  
-    while (1)
-    {
-        adc_value = read_ADC(0); // ler no PC0 que corresponde ao A0
-        //printf("%d\n", adc_value);
-        tmp = m * adc_value + b;
-        printf("%d\n", tmp);
+//     while (1)
+//     {
+//         adc_value = read_ADC(0); // ler no PC0 que corresponde ao A0
+//         //printf("%d\n", adc_value);
         
-        OCR1A = tmp; // 180 - um bocado por tentativa erro
-        
+    
+//         tmp = m * adc_value + b;
+//         printf("%d\n", tmp);
+//         OCR1A = tmp; // 180 - um bocado por tentativa erro
+
         
 
         
-    }
+//     }
     
-}
+// }
